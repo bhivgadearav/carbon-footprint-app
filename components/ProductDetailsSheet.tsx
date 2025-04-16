3da8b2a8ca6b94bed1cb331994aabc4f6a47946a
prathameshmahekar49 <mahekarprathu@gmail.com>
2025-04-16 23:30:09 +0530

    updated

--- components/ProductDetailsSheet.tsx
+++ components/ProductDetailsSheet.tsx
@@ -1,11 +1,9 @@
-import BottomSheet, { BottomSheetScrollView, BottomSheetView } from '@gorhom/bottom-sheet';
+import BottomSheet, { BottomSheetScrollView } from '@gorhom/bottom-sheet';
 import { useCallback, useRef } from 'react';
 import { Colors } from '@/constants/Colors';
 import { StyleSheet, useColorScheme } from 'react-native';
-import { ThemedText } from './ThemedText';
-import { ThemedView } from './ThemedView';
 import ProductDetails from './ProductDetails';
-import { ParsedProduct } from '@/app/scan';
+import { ParsedProduct } from './ProductDetails';
 
 export default function ProductDetailsSheet({ data }: { data: ParsedProduct }) {
 
@@ -17,7 +15,7 @@ export default function ProductDetailsSheet({ data }: { data: ParsedProduct }) {
     
     return (
         <BottomSheet
-        snapPoints={['48%', '100%']}
+        snapPoints={['48%', '15%']}
             ref={bottomSheetRef}
             onChange={handleSheetChanges}
             backgroundStyle={{ backgroundColor: theme === 'light' ? Colors.light.background : Colors.dark.background }}
@@ -25,18 +23,6 @@ export default function ProductDetailsSheet({ data }: { data: ParsedProduct }) {
         >
             <BottomSheetScrollView style={{flex: 1}}>
                 <ProductDetails product={data} />
-                {/* <ThemedView style={styles.contentContainer}>
-                    <ThemedText>Name: {data.name}</ThemedText>
-                    <ThemedText>Brand: {data.brand}</ThemedText>
-                    <ThemedText>Ingredients: {data.ingredients}</ThemedText>
-                    <ThemedText>Protein: {data.nutrition.protein.value} {data.nutrition.protein.unit}</ThemedText>
-                    <ThemedText>Saturated Fat: {data.nutrition.saturatedFat.value} {data.nutrition.saturatedFat.unit}</ThemedText>
-                    <ThemedText>Sugar: {data.nutrition.sugar.value} {data.nutrition.sugar.unit}</ThemedText>
-                    <ThemedText>Salt: {data.nutrition.salt.value} {data.nutrition.salt.unit}</ThemedText>
-                    <ThemedText>Calories: {data.nutrition.calories.value} {data.nutrition.calories.unit}</ThemedText>
-                    <ThemedText>Additives Count: {data.additives.count}</ThemedText>
-                    <ThemedText>Ingredients: {data.ingredients}</ThemedText>
-                </ThemedView> */}
             </BottomSheetScrollView>
         </BottomSheet>
     )
