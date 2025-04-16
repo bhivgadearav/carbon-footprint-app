82d9afb1232ecd79ca32ae08f53793c2d1b82326
Arav Bhivgade <148548538+bhivgadearav@users.noreply.github.com>
2025-04-16 11:29:19 +0530

    added base account screen

---
+++ app/(tabs)/account.tsx
@@ -0,0 +1,10 @@
+import { ThemedText } from "@/components/ThemedText";
+import { View } from "react-native";
+
+export default function AccountScreen() {
+    return (
+        <View>
+            <ThemedText>Account Screen</ThemedText>
+        </View>
+    )
+}
