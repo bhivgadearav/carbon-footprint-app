58398c26483a16a0e2ac8e12b44a6a4851c784f0
Arav Bhivgade <148548538+bhivgadearav@users.noreply.github.com>
2025-04-16 13:31:09 +0530

    minor changes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,13 +1,18 @@
 import { Stack } from "expo-router";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { AuthContext } from "@/context/AuthContext";
 
 export default function Layout() {
-    return <GestureHandlerRootView>
-        <Stack 
-        screenOptions={{
-            headerShown: false
-        }}
-        >
-        </Stack>
-    </GestureHandlerRootView>
+    return (
+        <AuthContext.Provider value={{ user: null, setUser: () => {}, signOut: () => {} }}>
+            <GestureHandlerRootView>
+                <Stack 
+                screenOptions={{
+                    headerShown: false
+                }}
+                >
+                </Stack>
+            </GestureHandlerRootView>
+        </AuthContext.Provider>
+    )
 }
