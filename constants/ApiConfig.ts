caf74d1d6431c764a2876cc21486cad9b7ca06f6
Arav Bhivgade <148548538+bhivgadearav@users.noreply.github.com>
2025-04-16 11:52:38 +0530

    added api config file

---
+++ constants/ApiConfig.ts
@@ -0,0 +1,3 @@
+export const API_BASE_URL = 'https://carbonfootprint1.p.rapidapi.com';
+export const API_KEY = '3276aacab1msh8c0f1fb442415c5p190e99jsn0428d656e68b';
+export const API_HOST = 'carbonfootprint1.p.rapidapi.com';
