0d76d514ce2a322f966dc4356cf2711e45cc6a9e
prathameshmahekar49 <mahekarprathu@gmail.com>
2025-04-16 23:29:10 +0530

    .

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -6,32 +6,38 @@ import { useColorScheme } from 'react-native';
 export default function TabLayout() {
   const theme = useColorScheme() ?? 'light';
   return (
-    <Tabs screenOptions={{ tabBarActiveTintColor: Colors[theme].tint, 
+    <Tabs screenOptions={{ 
+      tabBarActiveTintColor: "black", 
+      tabBarInactiveTintColor: "black",
       tabBarStyle: { 
-        backgroundColor: Colors[theme].background,
-        borderBlockStartColor: Colors[theme].background,
-       },
-    headerShown: false }}>
+      backgroundColor: "#ffff",
+      borderTopLeftRadius: 27,
+      borderTopRightRadius: 27,
+      paddingHorizontal: 10,
+      paddingVertical: 150,
+      },
+      headerShown: false 
+    }}>
       <Tabs.Screen
-        name="index"
-        options={{
-          title: 'Home',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={color} />,
-        }}
+      name="index"
+      options={{
+        title: 'Home',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="home" color={"black"} />,
+      }}
       />
       <Tabs.Screen
-        name="explore"
-        options={{
-          title: 'Explore',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="globe" color={color} />,
-        }}
+      name="explore"
+      options={{
+        title: 'Explore',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="globe" color={"black"} />,
+      }}
       />
       <Tabs.Screen
-        name="account"
-        options={{
-          title: 'Account',
-          tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={color} />,
-        }}
+      name="account"
+      options={{
+        title: 'Account',
+        tabBarIcon: ({ color }) => <FontAwesome size={28} name="user" color={"black"} />,
+      }}
       />
     </Tabs>
   );
